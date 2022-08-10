ee20e07530dcd608a8577b498d6607a094792d89
Lucas Veiga <lucasveigagonzaga@yahoo.com.br>
2022-08-10 09:32:08 -0300

    feat: creating modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,23 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
-import logoImg from '../../assets/logo.svg'
+import * as Dialog from "@radix-ui/react-dialog";
 
-export function Header(){
-    return (
-        <HeaderContainer>
-            <HeaderContent>
-        
-                <img src={logoImg}/>
-                <NewTransactionButton>
-                    Nova transação
-                </NewTransactionButton>
-            </HeaderContent>
-        </HeaderContainer>
-    )
+import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+         <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
