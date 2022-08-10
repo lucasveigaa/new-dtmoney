4d802925563d0a0d71a5d43f98186b7fc1dfc339
Lucas Veiga <lucasveigagonzaga@yahoo.com.br>
2022-08-10 03:15:05 -0300

    feat: Setting initial configurations and html and css for page transactions

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
+import { GLobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Transactions />
+
+      <GLobalStyle />
+    </ThemeProvider>
+  );
+}
+
+export default App;
